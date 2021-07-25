85e58d986c95d2e6c51ab313ddc258be300929bf
Gökdeniz Çetin <gokdenizcetintr@gmail.com>
2021-07-26 01:56:18 +0200

    Initial commit

---
+++ Assets/Scripts/Spotlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour
+{
+
+    private Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = this.gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+
+    }
+}
